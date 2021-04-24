ca946a7665faf85ac71cc8adc9fc45210a7e307b
Frithjof Petrick <fmpetrick@gmail.com>
2021-04-24 02:01:07 +0200

    Add fancier big ship

---
+++ Assets/Movement.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hi nochmal");
+        Debug.Log("Hi anton!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
