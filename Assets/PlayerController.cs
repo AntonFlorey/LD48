9e6e2c2c16e7200b9627cbd0013131678cc95dec
anton.florey <anton.florey@googlemail.com>
2021-04-24 18:29:53 +0200

    yay i can move

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -5,29 +5,50 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public int maxjumps = 1;
     public float moveSpeed;
-    public float jumpForce;
+    public float jumpForce = 1f;
+
+    private Rigidbody2D myBody;
+    private int jumpsLeft = 1;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        jumpsLeft = maxjumps;
+        myBody = this.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float x = Input.GetAxis("Horizontal");
-        float y = Input.GetAxis("Vertical");
-        transform.position += new Vector3(x, y, 0) * Time.deltaTime * moveSpeed;
+		if (Input.GetButtonDown("Jump") && jumpsLeft > 0)
+		{
+            Debug.Log("Jump Input detected");
+            Jump();
+		}
     }
 
-    private void Jump()
+	private void FixedUpdate()
 	{
-
+        myBody.velocity = new Vector3(moveSpeed * Input.GetAxis("Horizontal"), myBody.velocity.y);
 	}
-    
-    private void OnTriggerEnter2D(Collider2D other)
+
+	private void Jump()
+	{
+        myBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        jumpsLeft--;
+	}
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		if(collision.collider.tag == "ground")
+		{
+            jumpsLeft = maxjumps;
+		}
+	}
+
+	private void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log(other);   
     }
