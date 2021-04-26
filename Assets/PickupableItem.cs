8236773e21fd2e85558355c8220c5c2222f4b03b
Frithjof Petrick <fmpetrick@gmail.com>
2021-04-26 21:53:21 +0200

    item anim

--- Assets/PickupableItem.cs
+++ Assets/PickupableItem.cs
@@ -14,24 +14,44 @@ public class PickupableItem : MonoBehaviour
     public int healAmount = 0;
     public bool givesItem = false;
     public InventoryItem itemType = InventoryItem.Ignore;
+    public float floatFrequency = 0.5f;
+    public float floatRadius = 0;
+    public int spawnInAnimationDuration = 0;
+    private Vector3 originPos;
+    private int spawnInTimeLeft;
     
     private Rigidbody2D myBody;
-
     public void Start()
     {
         Assert.IsTrue(givesItem == (itemType != InventoryItem.Ignore));
         myBody = GetComponent<Rigidbody2D>();
+        originPos = transform.localPosition;
+        spawnInTimeLeft = spawnInAnimationDuration;
+        if (spawnInAnimationDuration > 0)
+            transform.localScale = Vector3.zero;
     }
 
     public void FixedUpdate()
     {
-        if (moveTowardsObject == null)
-            return;
-        var diff = moveTowardsObject.transform.position - transform.position;
-        Debug.DrawRay(transform.position, diff, Color.red, 2);
-        if (diff.sqrMagnitude <= maxMoveTowardsDistance * maxMoveTowardsDistance)
+        if (spawnInAnimationDuration > 0)
         {
-            myBody.AddForce(diff.normalized * moveTowardsForce);
+            spawnInTimeLeft = Math.Max(0, spawnInTimeLeft-1);
+            transform.localScale = (1 - ((float) spawnInTimeLeft) / spawnInAnimationDuration) * Vector3.one;
+        }
+
+        if (moveTowardsObject != null)
+        {
+            var diff = moveTowardsObject.transform.position - transform.position;
+            Debug.DrawRay(transform.position, diff, Color.red, 2);
+            if (diff.sqrMagnitude <= maxMoveTowardsDistance * maxMoveTowardsDistance)
+            {
+                myBody.AddForce(diff.normalized * moveTowardsForce);
+            }
+        }
+
+        if (floatRadius > 0)
+        {
+            transform.localPosition = originPos + floatRadius * Mathf.Sin(floatFrequency * Time.time) * Vector3.up;
         }
     }
 
